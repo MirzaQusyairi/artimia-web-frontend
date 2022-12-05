2165cd1fbc20c0ee487c32a5e0fe84474e6878f1
Mar'i Fauzan <marifauzan20@gmail.com>
2022-12-05 11:02:31 +0700

    initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Col, Row } from "antd";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+import amons from "../../assets/icon/amons.svg";
+import "./index.css";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function Home() {
+  const chartReference = useRef();
+  const [number, setNumber] = useState([]);
+  const [dataX, setDataX] = useState([]);
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        // data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        data: [],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  });
+
+  setInterval(() => {
+    const chart = chartReference.current;
+
+    chart.data.datasets[0].data.push(Math.random() * (3 - -3) + -3);
+    chart.data.labels.push(chart.data.labels.length + 1);
+    // setInterval(() => {
+
+    // }, 2000);
+    chart.update();
+  }, 100);
+
+  setInterval(() => {
+    const chart = chartReference.current;
+
+    chart.data.datasets[0].data.shift();
+    chart.data.labels.shift();
+
+    chart.update();
+  }, 150);
+
+  const options = {
+    responsive: true,
+    elements: {
+      line: {
+        tension: 0.1,
+      },
+      point: {
+        radius: 0,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <Row className="container">
+      <Col span={24}>
+        <Row justify="space-between">
+          <Col span={12}>
+            <img src={amons} alt="" />
+          </Col>
+          <Col span={12}>
+            <Row justify="end">
+              <Col>
+                <div className="name">Mar'i Fauzan Rambe</div>
+              </Col>
+            </Row>
+            <Row justify="end">
+              <Col>
+                <div className="age">21 Tahun</div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="content">
+          <Col span={19}>
+            <Row justify="center">
+              <Line
+                options={options}
+                ref={chartReference}
+                data={data}
+                height={150}
+              />
+            </Row>
+          </Col>
+          <Col span={5} className="table-status">
+            <Row justify="center">Status</Row>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+}
